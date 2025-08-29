0d778ca09cdfb484a88fea5c9a6c8e2054fad000
Akshay <hello@enterprise-onndemand.ai>
2025-08-29 23:44:29 +0530

    Allowed cors and new features added

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+// ✅ register cors before routes
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], // React dev server
+    credentials: true,
+  })
+);
+
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT || 5000, () =>
-      console.log(`Server running on port ${process.env.PORT || 5000}`)
-    );
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("MongoDB connection failed:", err));
