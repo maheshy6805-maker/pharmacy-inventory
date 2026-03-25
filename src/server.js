060d28af2fd25b51f94e0562d46ccf1322469500
maheshy6805-maker <maheshy6805@gmail.com>
2026-03-25 21:21:38 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ app.use(
       "http://localhost:5174",
       "http://127.0.0.1:5173",
       "https://app.pharmalogy.co.in", // vercel domain
+      "https://app.krishnamedicose.in",
     ],
     credentials: true,
   })
